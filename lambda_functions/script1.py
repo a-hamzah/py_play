c2cb646941384c6b2f07b465a5e7a34089fce93a
a-hamzah <Spellbreaker907@gmail.com>
2024-09-02 15:51:58 +0500

    lambda_reduce

--- lambda_functions/script1.py
+++ lambda_functions/script1.py
@@ -1,4 +1,5 @@
 import math
+from functools import reduce
 # def square(x): return x**2
 
 
@@ -9,6 +10,10 @@ import math
 # print(result1)
 # result = square(2)
 # print(result)
+
+
+# ---- map() function -----
+# map() function applies given function to each element of the list
 
 items = [1, 2, 3, 4, 5]
 
@@ -28,3 +33,23 @@ sqrt_list = list(map(lambda x: math.sqrt(x), whole_num))
 
 print(sqrt_list)
 # print(whole_num_sqrt)
+
+# ---  filter() function ----
+# filter() function creates a list of elements for which function is True
+filter_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+iter_list = []
+for i in range(10):
+    iter_list.append(i)
+print(filter_list)
+print(iter_list)
+res1 = list(filter(lambda x: x % 2 == 0, filter_list))
+print(res1)
+res2 = list(filter(lambda x: x % 2 == 0, iter_list))
+print(res2)
+
+
+# ---  reduce() function ----
+
+original_list = [1, 2, 3, 4, 5]
+result = reduce(lambda x, y: x * y, original_list)
+print(result)
